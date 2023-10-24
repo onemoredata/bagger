779ee3f424dcc3856bae6c6ba349d26b70c0c714
Chris Travers <chris.travers@gmail.com>
2023-10-24 19:44:48 +0700

    Key/field mapper for LW Agent
    This required some refactoring because many of our current modules
    could not be loaded together.  This fixes this as well.  As well
    as some other minor issues probably from past rebases.

---
+++ lib/Bagger/Agent/Storage/Mapper.pm
@@ -0,0 +1,154 @@
+=head1 NAME
+
+   Bagger::Agent::Storage::Mapper -- Postgres <-> KVStore Mappers for Agents
+
+=cut
+
+package Bagger::Agent::Storage::Mapper;
+
+=head1 SYNOPSIS
+
+   # To etcd
+   use Bagger::Agent::Storage::Mapper 'kval_key';
+
+   my $key = kval_key('config', $config_hash);
+
+   #or
+
+   my $key = kval_key($object);
+
+   # from etcd
+   use Bagger::Agent::Storage::Mapper 'pg_obj';
+   my $class = pg_obj($etcd_key);
+   # or even
+   my $obj = pg_obj($etcd_key)->new($etcd_value);
+
+=cut
+
+use strict;
+use warnings;
+use Exporter 'import';
+use Scalar::Util 'blessed';
+our @EXPORT_OK = qw(kval_key pg_object);
+
+=head1 DESCRIPTION
+
+This module provides modules for mapping Postgres and key/value store  data.
+This mapping happens between objets or between table names/hash data and the
+key/value store key.  A key is also sufficient to determine the class.
+
+This module is written to ensure that we can store the configuratin structures
+for the cluster in a key/value store such as Zookeeper, etcd or Consul.  It is
+possible that some minor changes may be required to support more of these in
+the future.
+
+When loading data from etcd, this module also loads classes when they are
+needed.  This makes it easy to use in this regard and if an agent only needs to
+watch for some items, those types will be loaded only as needed.
+
+=cut
+
+# Once we add Perl::Critic tests, these will likely need some comments
+# disabling policies due to the use of @_ directly.
+
+my $delim = '/';
+
+my @key_regex = (
+    { regex => qr|^/Config|,           class => 'Bagger::Storage::Config' },
+    { regex => qr|^/PostgresInstance|, class => 'Bagger::Storage::Instance' },
+    { regex => qr|^/Servermap|,        class => 'Bagger::Storage::Servermap' },
+    { regex => qr|^/Dimension|,        class => 'Bagger::Storage::Dimension' },
+    { regex => qr|^/Index${delim}\d+${delim}\d|,     
+                                       class => 'Bagger::Storage::Index::Field' },
+    { regex => qr|^/Index${delim}\d+$|,       class => 'Bagger::Storage::Index' },
+);
+
+sub _kjoin { return join($delim, @_) }
+
+my %keygen = (
+    config             => sub { return _kjoin('/Config', $_[0]->{key}) },
+    postgres_instances => sub { return _kjoin('/PostgresInstance', 
+                                              $_[0]->{host}, $_[0]->{port}) },
+    servermaps         => sub { return '/Servermap' },
+    dimensions         => sub { return _kjoin('/Dimension', $_[0]->{id}) },
+    indexes            => sub { return _kjoin('/Index', $_[0]->{id}) },
+    index_fields       => sub { return _kjoin('/Index', $_[0]->{index_id},
+                                              $_[0]->{id}) },
+);
+
+my %classmap = (
+    'Bagger::Storage::Config'       => 'config',
+    'Bagger::Storage::Instance'     => 'postgres_instances',
+    'Bagger::Storage::Servermap'    => 'servermaps', 
+    'Bagger::Storage::Dimension'    => 'dimensions',
+    'Bagger::Storage::Index::Field' => 'index_fields',
+    'Bagger::Storage::Index'        => 'indexes',
+);
+
+=head1 FUNCTIONS AND CONVERSION
+
+The delimiter is currently set to '/' so key fields are separated by that
+character.
+
+The keys form a tree starting with '/' so if one watches for all keys with a
+prefix of '/', the agent will get all bagger-related changes.
+
+=head2 pg_object
+
+This returns the class which the key is associated.  The value can then be
+converted from JSON into Perl data structures and used to create an object of
+this class.  The class is also autoloaded once it is identified.
+
+This function will return undef if no class is found.
+
+=cut
+
+my %loaded = ();
+
+
+# Note compiled regexes are significantly slower than static ones but
+# this shouldn't matter since this is not frequently changing data.
+sub pg_object {
+    my ($key) = shift;
+    for my $r (@key_regex) {
+        if ($key =~ $r->{regex}){
+            my $class = $r->{class};
+            if (not $loaded{$class}){
+                local $@;
+                eval "require $class";
+                warn $@ if $@;
+                $loaded{$class} = 1;
+            }
+            return $class;
+        }
+    }
+    return undef;
+}
+
+=head2 kval_key
+
+This returns the key/val storekey from the object.  This requires either a
+table name or a blessed object passed in the first spot, and if not, then a hash
+(blessed or not, but usually not) passed in the second place.
+
+Examples:
+
+    kval_key('postgres_instances', {host => 'host1', 
+                                    port => 5432, 
+                                username => 'bagger',
+                                  status => 0 })
+    #returns '/PostgresInstance/host1/5432'
+
+
+=cut
+
+sub kval_key {
+    my ($class, $val) = @_;
+    if (ref $class) {
+        $val = $class;
+        $class = $classmap{blessed $class};
+    }
+    return $keygen{$class}($val) if defined $keygen{$class};
+}
+
+1;
---
+++ t/12-kvstore-keygen.t
@@ -0,0 +1,57 @@
+use Test2::V0 -target => { inst => 'Bagger::Storage::Instance',
+                           smap => 'Bagger::Storage::Servermap',
+                           jtyp => 'Bagger::Type::JSON',
+                           dtyp => 'Bagger::Type::DateTime',
+                           dim  => 'Bagger::Storage::Dimension',
+                           idx  => 'Bagger::Storage::Index',
+                           fld  => 'Bagger::Storage::Index::Field'};
+use Bagger::Agent::Storage::Mapper qw(kval_key pg_object);
+use strict;
+use warnings;
+plan 15;
+
+is(pg_object('/Servermap'), smap(), 'Servermap object determined');
+is(pg_object('/PostgresInstance/host1/5432'), inst(), 'PG Instance key read');
+is(pg_object('/Message'), undef, 'Non-existent key returns undef');
+is(pg_object('/Dimension/1'), dim(), 'Dimension key found');
+is(pg_object('/Index/1/1'), fld(), 'Index Field Found');
+is(pg_object('/Index/1'), idx(), 'Index found');
+
+# Table types
+is(kval_key('postgres_instances', {host => 'host1', port => '5432'}),
+        '/PostgresInstance/host1/5432',
+        'kval_key for postgres_instance record');
+
+is(kval_key('servermaps', {id => 1}), '/Servermap',
+       'kval_key for servermaps record');
+
+is(kval_key('dimensions', {id => 1}), '/Dimension/1',
+       'kval_key for dimensions record');
+
+is(kval_key('indexes', {id => 1}), '/Index/1',
+       'kval_key for indexes record');
+
+is(kval_key('index_fields', {id => 1, index_id => 2}), '/Index/2/1',
+        'kval_key for index_fields record');
+
+## Object types
+is(kval_key(inst()->new(host => 'host1', port => 5432, 
+                     username => 'foo', status => 0)), 
+             '/PostgresInstance/host1/5432',
+             'Got correct key for Postgres Instance');
+
+is(kval_key(fld()->new(ordinality => 1, id => 1, 
+            index_id => 1, expression =>'foo()',
+            valid_from => dtyp()->inf_past,
+            valid_until=> dtyp()->inf_future)),
+    '/Index/1/1', 'Index Field Key Correct');
+
+is(kval_key(idx()->new(id => 1, indexname => 'i', access_method => 'gin',
+            valid_from => dtyp()->inf_past,
+            valid_until=> dtyp()->inf_future)),
+    '/Index/1', 'Index Key Correct');
+
+is(kval_key(smap()->new(id => 1, server_map => jtyp()->new({ version => 1 }))),
+        '/Servermap',
+        'Server Map Key Found');
+
